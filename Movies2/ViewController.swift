6c1ffac1d6b77429afe79889bd05fe2687677da8
gog1na <82454907+gog1na@users.noreply.github.com>
2023-01-17 12:10:07 +0400

    Initial commit

---
+++ Movies2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies2
+//
+//  Created by Giorgi Goginashvili on 1/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
